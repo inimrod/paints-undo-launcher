5a65fb5b354c46660ae7e6cb11403e7ea4d44d50
iNimrod <brodlefleur@gmail.com>
2024-08-04 14:38:54 +0800

    Initial version

---
+++ README.md
@@ -0,0 +1,8 @@
+# bark-simple-webui
+Installer and launcher for [Paints-UNDO](https://github.com/lllyasviel/Paints-UNDO) on GNU/Linux system.
+
+# Usage
+1. Install [miniconda](https://docs.anaconda.com/miniconda/#quick-command-line-install).
+1. Clone this repo.
+1. Update vars in `run.sh`.
+1. Run `run.sh`.
---
+++ run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+##### SET FOLLOWING VARS #####
+CONDA_DIR="/home/$USER/src/miniconda3"
+SERVER_NAME="192.168.0.187"
+###### END OF VARIABLES ######
+
+
+# source base conda env in this script's subshell
+# https://github.com/conda/conda/issues/7980
+source $CONDA_DIR/etc/profile.d/conda.sh
+
+# set working dir to this script's location:
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+INSTALL_DIR="$SCRIPT_DIR/Paints-UNDO"
+CONDA_ENV_DIR="$SCRIPT_DIR/conda_env"
+
+available() { command -v $1 >/dev/null; }
+
+cd $SCRIPT_DIR
+
+# check if miniconda already installed
+if ! available conda; then
+    echo "miniconda is not yet installed. Install it first."
+    exit 0
+fi
+
+# check if conda env already exists, create if not yet done
+if [ ! -d "$CONDA_ENV_DIR" ]; then
+    conda create --no-shortcuts -y -k --prefix "$CONDA_ENV_DIR" python=3.10
+fi
+
+# confirm if conda env is actually created
+if [ ! -f "$CONDA_ENV_DIR/bin/python" ]; then
+    echo "python is not found in $CONDA_ENV_DIR/bin"
+    echo "exiting."
+    exit 0
+fi
+
+# activate conda env
+conda activate $CONDA_ENV_DIR || echo "Miniconda hook not found."
+
+# check if install dir already exists
+if [ ! -d "$INSTALL_DIR" ]; then
+    git clone https://github.com/lllyasviel/Paints-UNDO.git
+    cd Paints-UNDO
+    pip install xformers
+    pip install -r requirements.txt
+fi
+
+# launch
+export GRADIO_SERVER_NAME=$SERVER_NAME
+python gradio_app.py
